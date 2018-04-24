5ea41b5df32f807d889f2ba806aeb9cf13635871
Yoni MALINE <yoni.maline14gmail.com>
2018-04-24 11:37:33 +0200

    CA MARCHE ?

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculLibrary;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsNotNull(Calcul.GetInt("20"));
+            Assert.AreEqual(Calcul.GetInt("40"), 40);
+            Assert.AreEqual(Calcul.GetInt("50"), 50);
+            Assert.AreEqual(Calcul.GetInt("1000"), 1000);
+        }
+    }
+}
