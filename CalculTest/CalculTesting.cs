11d2c000c30d8c832eba0fac20f7c080c59c1711
Yoni MALINE <yoni.maline14gmail.com>
2018-04-24 10:57:07 +0200

    Ajout d'un test unitaire

---
+++ CalculTest/CalculTesting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculLibrary;
+
+namespace CalculTest
+{
+    [TestClass]
+    public class CalculTesting
+    {
+        [TestMethod]
+        public void TestGetInt()
+        {
+            string number = "42";
+
+            Assert.AreEqual(42, Calcul.GetInt(number));
+        }
+    }
+}
